8212e9e73c7e5d40af493ba7f3e13c23e3125f5e
u902192 <crgarcete@bancor.com.ar>
2024-03-21 14:55:58 -0300

    added pysikuli test

--- imageAutomation/test-image.py
+++ imageAutomation/test-image.py
@@ -1,11 +1,18 @@
-import pyautogui
+import pysikuli as sik
+from pysikuli import Key, Button, Region, config
+import time
+
+sik.mouseMove((100,100))
+
+configuracionAvanzadaPicture="C:/proyecots_personales/web-test-automation/imageAutomation/configuracionAvanzada.png"
+accederAwww="C:/proyecots_personales/web-test-automation/imageAutomation/accederAwww.png"
+
+findd = sik.find(image=configuracionAvanzadaPicture, max_search_time=5)
+print("valor: ",findd)
 
 
-path = "buscador.png"
-
-pyautogui.click(path)
-
-
-
-#pyautogui.moveTo(x, y, duration=1)
-
+sik.click(configuracionAvanzadaPicture)
+time.sleep(1)
+sik.click(accederAwww)
+time.sleep(10)
+#Click "Activar advertencias"
