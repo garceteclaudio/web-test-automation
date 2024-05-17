75c1a75852a61812ec976c676c0215d2a3e4fd85
Claudio Rodolfo Garcete <claudiorodolfogarcete@MacBook-Pro-de-Claudio.local>
2024-05-17 11:16:04 -0300

    fix behave.ini and example waits

--- features/steps/login.steps.py
+++ features/steps/login.steps.py
@@ -2,27 +2,30 @@
 from behave import *
 import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.wait import WebDriverWait
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 @given(u'Launch chrome browser')
 def step_impl(context):
     print("Holaaa")
     context.driver = webdriver.Chrome()
-    # implicit wait
     context.driver.implicitly_wait(5)
     context.driver.get('https://www.saucedemo.com/')
-    
+
+
+
 
 
 @when(u'I type my user "{user}"')
 def step_impl(context, user):
     # Wait explicito
-    wait = WebDriverWait(context.driver, timeout=5)
-    elem = context.driver.find_element(By.ID, 'user-name')
-    wait.until(lambda d : elem.is_displayed())
-    elem.send_keys(user)
+    wait = WebDriverWait(context.driver, timeout=15)
+    element = wait.until(EC.visibility_of_element_located((By.ID, "user-name")))
+    element.send_keys(user)
 
 
 @when(u'my password')
